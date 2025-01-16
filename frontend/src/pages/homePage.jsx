f2dd09c48c9e177098bd55659438a7be2a712b86
akimmk <damtewmikiyas@gmail.com>
2025-01-16 21:32:10 +0300

    tailwind config

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,6 +1,5 @@
 function HomePage() {
-    return <h1>Welcome to the Home Page!</h1>;
-  }
-  
-  export default HomePage;
-  
+  return <h1 className="font-bold text-blue-500">Welcome to the Home Page!</h1>;
+}
+
+export default HomePage;
